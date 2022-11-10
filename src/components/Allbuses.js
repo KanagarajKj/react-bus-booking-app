ae5a1272ee1ecf36b801afa0064c2e41c33a8f65
kanagaraj M <kanagaraj.m@iinerds.com>
2022-11-10 12:12:28 +0530

    Fourth Commit

--- src/components/Allbuses.js
+++ src/components/Allbuses.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import BusDetails from './BusDetails';
 
 const AllBuses = () => {
-  const { busDetails } = useSelector((state) => state.bus);
   return (
     <>
       <div className="title">
         <h3>ALL BUSES</h3>
       </div>
-      <BusDetails busDetails={busDetails}/>
+      <BusDetails/>
     </>
   );
 };
